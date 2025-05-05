9d7ce2e8e879bb221d1fb5f277cf9cea70e690e5
memoTux <romeo@mendezfuentes.net>
2025-05-05 08:24:48 -0600

    add content

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <header>
+    <h1>Test Nitro Preset AWS Amplify Static</h1>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/post">Post 1</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </nav>
+  </header>
+  <main>
+    <NuxtPage />
+  </main>
+  <footer>
+    <p>
+      Developed by:
+      <NuxtLink
+        to="https://github.com/memotux"
+        external
+        >@memotux</NuxtLink
+      >
+    </p>
+  </footer>
 </template>
+
+<style lang="css">
+nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 25dvw;
+}
+main {
+  min-height: 80dvh;
+}
+</style>
---
+++ components/AppTitle.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+const route = useRoute()
+</script>
+
+<template>
+  <h1>This is {{ route.name?.toString().toLocaleUpperCase() }} page</h1>
+</template>
