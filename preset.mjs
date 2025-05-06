594a5a2922c8f87d7acb905b68ccb5ecb63fc815
memoTux <romeo@mendezfuentes.net>
2025-05-05 20:59:51 -0600

    add preset.mjs

---
+++ preset.mjs
@@ -0,0 +1,50 @@
+import { defineNitroPreset } from 'nitropack/kit'
+import { writeAmplifyFiles } from './utils.mjs'
+const awsAmplify = defineNitroPreset(
+  {
+    extends: 'node-server',
+    entry: './runtime/aws-amplify',
+    output: {
+      dir: '{{ rootDir }}/.amplify-hosting',
+      serverDir: '{{ output.dir }}/compute/default',
+      publicDir: '{{ output.dir }}/static{{ baseURL }}',
+    },
+    commands: {
+      preview: 'node ./compute/default/server.js',
+    },
+    hooks: {
+      async compiled(nitro) {
+        await writeAmplifyFiles(nitro)
+      },
+    },
+  },
+  {
+    name: 'aws-amplify',
+    stdName: 'aws_amplify',
+    url: import.meta.url,
+  }
+)
+const awsAmplifyStatic = defineNitroPreset(
+  {
+    extends: 'aws-amplify',
+    static: true,
+    prerender: {
+      crawlLinks: true,
+    },
+    serveStatic: false,
+    output: {
+      dir: '{{ rootDir }}/.amplify-hosting',
+      publicDir: '{{ output.dir }}/static{{ baseURL }}',
+    },
+    commands: {
+      preview: 'npx serve ./static',
+    },
+  },
+  {
+    name: 'aws-amplify-static',
+    stdName: 'aws_amplify',
+    static: true,
+    url: import.meta.url,
+  }
+)
+export default [awsAmplify, awsAmplifyStatic]
